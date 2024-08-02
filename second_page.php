cb6c51ab96e1e63b3f392bf0a63c85c3358b66b6
JamesPotenciano <146494940+JamesPotenciano@users.noreply.github.com>
2024-08-02 20:46:26 +0800

    Add files via upload

---
+++ second_page.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+
+    <title>MYKOPIKO</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <h5>WALA PA LAMAN</h5>
+    <h4>DITO YUNG MENU</h4>
+</body>
+</html>
